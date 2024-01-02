7b826278c82dfb7e43cf4f64d9587f92f0721321
adel alfajji <natalikagami@gmail.com>
2024-01-02 18:21:50 +0100

    test

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,16 @@
 import React, {useReducer} from "react";
 
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: string;
+}
+
+type Action =
+  | { type: "START_REQUEST" }
+  | { type: "PENDING_REQUEST" }
+  | { type: "FINISH_REQUEST" }
+  | { type: "RESET_REQUEST" };
+  
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
